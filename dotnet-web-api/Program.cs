104a5e2574404827fc618314e4b9e323da776ba2
aksho-sh <akshobhyasharma@yahoo.com>
2025-03-02 03:25:40 +0000

    Added dockerfile for the dotnet api

--- dotnet-web-api/Program.cs
+++ dotnet-web-api/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Force ASP.NET Core to listen on port 5000
+builder.WebHost.UseUrls("http://+:5000");
+
 var app = builder.Build();
-
-app.MapGet("/", ()=>("Hello World"));
-
+app.MapGet("/", () => "Hello from .NET API!");
 app.Run();
